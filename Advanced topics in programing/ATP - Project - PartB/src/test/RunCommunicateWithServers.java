1c9fb45839e5c3e3a28369f284ef7b45ebe9f2ee
onagar <omer.nagar@intel.com>
2019-05-15 21:09:57 +0300

    test 2/6 fails

--- Advanced topics in programing/ATP - Project - PartB/src/test/RunCommunicateWithServers.java
+++ Advanced topics in programing/ATP - Project - PartB/src/test/RunCommunicateWithServers.java
@@ -53,7 +53,7 @@ public class RunCommunicateWithServers {
                         toServer.flush();
                         byte[] compressedMaze = (byte[]) fromServer.readObject(); //read generated maze (compressed with MyCompressor) from server
                         InputStream is = new MyDecompressorInputStream(new ByteArrayInputStream(compressedMaze));
-                        byte[] decompressedMaze = new byte[2530 /*CHANGE SIZE ACCORDING TO YOU MAZE SIZE*/]; //allocating byte[] for the decompressed maze -
+                        byte[] decompressedMaze = new byte[1000 /*CHANGE SIZE ACCORDING TO YOU MAZE SIZE*/]; //allocating byte[] for the decompressed maze -
                         is.read(decompressedMaze); //Fill decompressedMaze with bytes
                         Maze maze = new Maze(decompressedMaze);
                         maze.print();
@@ -62,7 +62,7 @@ public class RunCommunicateWithServers {
                     }
                 }
             });
-            client.start();
+            client.communicateWithServer();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
@@ -95,7 +95,7 @@ public class RunCommunicateWithServers {
                     }
                 }
             });
-            client.start();
+            client.communicateWithServer();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
@@ -124,7 +124,7 @@ public class RunCommunicateWithServers {
                     }
                 }
             });
-            client.start();
+            client.communicateWithServer();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
