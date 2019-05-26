edc8d4baa3c65ca6bb4f69412c0b145a11615081
AsiZaks <49097451+AsiZaks@users.noreply.github.com>
2019-05-26 15:01:39 +0300

    did some work on view

--- Advanced topics in programing/ATP - Project - PartC/src/View/MyViewController.java
+++ Advanced topics in programing/ATP - Project - PartC/src/View/MyViewController.java
@@ -1,4 +1,10 @@
 package View;
 
+import javafx.event.ActionEvent;
+
 public class MyViewController implements IView{
+    public void preesButton(ActionEvent event){
+        System.out.println("Hellsssaa");
+    }
+
 }
