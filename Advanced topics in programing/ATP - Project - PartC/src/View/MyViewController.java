e72374a2a5f82bad9ee9cf4ef409f3ce7d42228b
AsiZaks <49097451+AsiZaks@users.noreply.github.com>
2019-05-27 09:07:05 +0300

    did some work on view

--- Advanced topics in programing/ATP - Project - PartC/src/View/MyViewController.java
+++ Advanced topics in programing/ATP - Project - PartC/src/View/MyViewController.java
@@ -1,10 +1,32 @@
 package View;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MyViewController implements IView{
     public void preesButton(ActionEvent event){
         System.out.println("Hellsssaa");
+    }
+
+    public void pressNew(ActionEvent newMaze){
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../View/NewGame.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("New Game");
+            stage.setScene(new Scene(root, 400, 300));
+            stage.show();
+            // Hide this current window (if this is what you want)
+            //((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
